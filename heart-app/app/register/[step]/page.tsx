ad7e6047f5c934ecd1637a3614bc7194562e1289
AndersonMoura <74934630+Anderson0312@users.noreply.github.com>
2025-03-19 22:02:57 -0300

    Update page.tsx

--- heart-app/app/register/[step]/page.tsx
+++ heart-app/app/register/[step]/page.tsx
@@ -374,7 +374,7 @@ const RegisterStep = () => {
                     id="date"
                     name="date"
                     type="datetime-local"
-                    value={formData.date ? formData.date.toISOString().slice(0, 16) : ''} 
+                    value={formData.date ? formData.date.toLocaleString('sv-SE').slice(0, 16) : ''} 
                     onChange={handleDateChange}
                     className="block w-full rounded-md bg-gray-700 px-3 py-2 text-base text-white placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-red-600 sm:text-sm pl-10"
                     style={{ width: '100%' }}

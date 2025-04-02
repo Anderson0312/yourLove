cb5094ead64b45836f95c991aa564067f2d8ad75
AndersonMoura <andersonmoura812@gmail.com>
2025-04-02 11:47:31 -0300

    Update TextDatting.tsx

--- heart-app/components/TextDatting.tsx
+++ heart-app/components/TextDatting.tsx
@@ -8,10 +8,11 @@ interface TextDatingProps {
 export default function TextDating({ name, text }: TextDatingProps) {
   return (
       <section className="relative w-full max-w-3xl mx-auto w-full p-6 text-center  sm:p-10 max-w-md text-center "
-      style={{ fontFamily: "'Dancing Script', serif", fontStyle: "italic" }}>
+      style={{ fontFamily: "'Playfair Display', serif" }}>
         <h2
           className="text-2xl sm:text-3xl font-semi-bold text-red-600 mb-4 italic"
-          style={{ fontFamily: "'Playfair Display', serif" }}
+          
+          style={{ fontFamily: "'Dancing Script', serif", fontStyle: "italic" }}
         >
           {name || "Seus Nomes"}
         </h2>

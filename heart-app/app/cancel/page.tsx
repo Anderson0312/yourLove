67da49fdb0d7a8d2747ece1798f0ede98b927745
AndersonMoura <74934630+Anderson0312@users.noreply.github.com>
2025-04-21 23:06:40 -0300

    Update page.tsx

--- heart-app/app/cancel/page.tsx
+++ heart-app/app/cancel/page.tsx
@@ -5,7 +5,7 @@ export default function CancelPage() {
       <div className="flex flex-col items-center justify-center h-screen text-center">
         <h1 className="text-3xl font-bold text-red-600">Pagamento cancelado ❌</h1>
         <p className="mt-4 text-lg">O pagamento foi cancelado. Você pode tentar novamente quando quiser.</p>
-        <Link  href="/" className="mt-6 px-4 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 transition">
+        <Link  href="/register/5" className="mt-6 px-4 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 transition">
           Voltar para a página inicial
         </Link >
       </div>
